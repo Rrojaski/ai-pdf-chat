554e48b4c0f49549ed302e11a93a3043564682f0
Roman Rojas <rrojaski@gmail.com>
2024-12-11 15:29:28 -0500

    implement PDF file upload and vector store index creation

--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -1,11 +1,34 @@
 import { NextResponse } from "next/server";
 import PDF from "@cyber2024/pdf-parse-fixed";
+import { createOrReadVectorStoreIndex } from "@/lib/vector-store";
 
 export async function POST(request: Request) {
-  const formData = await request.formData();
-  console.log("formData ", formData);
-  const file = formData.get("file") as File;
-  const fileContent = await file.arrayBuffer();
+  try {
+    const formData = await request.formData();
+    console.log("formData ", formData);
+    const file = formData.get("file") as File;
+    const fileContent = await file.arrayBuffer();
 
-  const parsedPdf = await PDF(Buffer.from(fileContent));
+    const parsedPdf = await PDF(Buffer.from(fileContent));
+
+    await createOrReadVectorStoreIndex(parsedPdf.text);
+
+    return NextResponse.json(
+      {
+        message: "File uploaded",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
